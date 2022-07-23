fca6fca54238313a7b3c6a5712d080ff0a628cb3
harukaeru <nekoneko.myaomyao@gmail.com>
2022-07-24 01:26:07 +0900

    check more

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+cache = set()
+
 while True:
     try:
         x = input()
@@ -5,4 +7,9 @@ while True:
         print('ok')
         exit()
     a = list(map(int, x.split()))
+
+    t = tuple(sorted(a))
+    if t in cache:
+        raise Exception('Already existed', t)
+    cache.add(t)
     assert a[0] * a[0] + a[1] * a[1] + a[2] * a[2] == 3 * a[0] * a[1] * a[2], a
